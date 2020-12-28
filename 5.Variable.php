5c8798dbbbba532464000c35144fbf361286ced3
Hafet17 <do.crazy192@gmail.com>
2020-12-29 01:56:11 +0700

    add 5

---
+++ 5.Variable.php
@@ -0,0 +1,19 @@
+<?php
+
+$name = "Hafet";
+$age = 22;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo $age;
+echo "\n";
+
+$contoh = "hafid";
+$$contoh = "masruri";
+
+echo "Contoh : ";
+echo $contoh;
+echo "\n";
