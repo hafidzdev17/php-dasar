b7476231b04f9560f8d54b0bb5b8cdfd7bb220f8
Hafet17 <do.crazy192@gmail.com>
2020-12-29 01:55:59 +0700

    add 4

---
+++ 4.String.php
@@ -0,0 +1,14 @@
+<?php
+echo 'Name : ';
+echo 'M Hafidz Masruri';
+echo "\n";
+
+echo "Name : ";
+echo "M\t Hafidz\t Masruri\n";
+
+echo <<<HAFID
+Selamat belajar PHP
+sekarang, kita belajar tipe data string
+ini adalah cara ke-3 membuat string
+bisa menggunakan heredoc
+HAFID;
