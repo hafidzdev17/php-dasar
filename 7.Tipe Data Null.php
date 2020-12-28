d46d3e469114142e021677e99c7f29666aaa9f3e
Hafet17 <do.crazy192@gmail.com>
2020-12-29 01:56:30 +0700

    add 7

---
+++ 7.Tipe Data Null.php
@@ -0,0 +1,25 @@
+<?php
+$name = "Hafid";
+$name = null;
+
+$age = null;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo $age;
+echo "\n";
+
+echo "Is Name Null? : ";
+var_dump(is_null($name));
+echo "\n";
+
+$contoh = "Hafid";
+unset($contoh);
+
+$contoh = "Kurniawan";
+$contoh = null;
+
+var_dump(isset($contoh));
